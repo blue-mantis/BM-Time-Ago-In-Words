2da2b89c415fd27039fb4a9afe718596cfe051af
Maarten de Graaf <maarten@webtraders.nl>
2015-11-20 00:44:20 +0100

    Add multi-language support and include Dutch translations

--- timeagoinwords/services/TimeAgoInWords_FunctionsService.php
+++ timeagoinwords/services/TimeAgoInWords_FunctionsService.php
@@ -34,50 +34,50 @@ class TimeAgoInWords_FunctionsService extends BaseApplicationComponent
 		if ( $distanceInMinutes <= 1 ) {
 
 			if ( $distanceInSeconds < 5 ) {
-				return 'less than 5 seconds';
+				return Craft::t('less than 5 seconds');
 			}
 			if ( $distanceInSeconds < 10 ) {
-				return 'less than 10 seconds';
+				return Craft::t('less than 10 seconds');
 			}
 			if ( $distanceInSeconds < 20 ) {
-				return 'less than 20 seconds';
+				return Craft::t('less than 20 seconds');
 			}
 			if ( $distanceInSeconds < 40 ) {
-				return 'about half a minute';
+				return Craft::t('about half a minute');
 			}
 			if ( $distanceInSeconds < 60 ) {
-				return 'less than a minute';
+				return Craft::t('less than a minute');
 			}
 
-			return '1 minute';
+			return Craft::t('1 minute');
 
 		}
 		if ( $distanceInMinutes < 45 ) {
-			return $distanceInMinutes . ' minutes';
+			return Craft::t('{amount} minutes', array('amount' => $distanceInMinutes));
 		}
 		if ( $distanceInMinutes < 90 ) {
-			return 'about 1 hour';
+			return Craft::t('about 1 hour');
 		}
 		if ( $distanceInMinutes < 1440 ) {
-			return 'about ' . round(floatval($distanceInMinutes) / 60.0) . ' hours';
+			return Craft::t('about {amount} hours', array('amount' => round(floatval($distanceInMinutes) / 60.0)));
 		}
 		if ( $distanceInMinutes < 2880 ) {
-			return '1 day';
+			return Craft::t('1 day');
 		}
 		if ( $distanceInMinutes < 43200 ) {
-			return 'about ' . round(floatval($distanceInMinutes) / 1440) . ' days';
+			return Craft::t('about {amount} days', array('amount' => round(floatval($distanceInMinutes) / 1440)));
 		}
 		if ( $distanceInMinutes < 86400 ) {
-			return 'about 1 month';
+			return Craft::t('about 1 month');
 		}
 		if ( $distanceInMinutes < 525600 ) {
-			return round(floatval($distanceInMinutes) / 43200) . ' months';
+			return Craft::t('{amount} months', array('amount' => round(floatval($distanceInMinutes) / 43200)));
 		}
 		if ( $distanceInMinutes < 1051199 ) {
-			return 'about 1 year';
+			return Craft::t('about 1 year');
 		}
 
-		return 'over ' . round(floatval($distanceInMinutes) / 525600) . ' years';
+		return Craft::t('over {amount} years', array('amount' => round(floatval($distanceInMinutes) / 525600)));
 	}
 
 }
---
+++ timeagoinwords/translations/nl.php
@@ -0,0 +1,30 @@
+<?php
+
+return array(
+	// seconds
+	'less than 5 seconds' => 'minder dan 5 seconden',
+	'less than 10 seconds' => 'minder dan 10 seconden',
+	'less than 20 seconds' => 'minder dan 20 seconden',
+
+	// minutes
+	'less than half a minute' => 'minder dan een halve minuut',
+	'less than a minute' => 'minder dan een minuut',
+	'1 minute' => '1 minuut',
+	'{amount} minutes' => '{amount} minuten',
+
+	// hours
+	'about 1 hour' => 'ongeveer 1 uur',
+	'about {amount} hours' => 'ongeveer {amount} uur',
+
+	// days
+	'1 day' => '{amount} dag',
+	'about {amount} days' => 'ongeveer {amount} dagen',
+
+	// months
+	'about 1 month' => 'ongeveer 1 maand',
+	'{amount} months' => '{amount} maanden',
+
+	// years
+	'about 1 year' => 'ongeveer 1 jaar',
+	'over {amount} years' => 'meer dan {amount} jaar',
+);
